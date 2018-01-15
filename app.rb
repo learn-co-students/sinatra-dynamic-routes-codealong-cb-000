10c763e244b6446b13dcdecbdd4b23b980309784
Marcus Havranek <marcus.havranek@havranek.co.at>
2018-01-15 18:39:34 +0100

    Done.

--- app.rb
+++ app.rb
@@ -15,4 +15,15 @@ class App < Sinatra::Base
 
   # Code your final two routes here:
 
+  get '/goodbye/:name' do
+    @name = params[:name]
+    erb :goodbye
+  end
+
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    erb :multiply
+  end
+
 end
