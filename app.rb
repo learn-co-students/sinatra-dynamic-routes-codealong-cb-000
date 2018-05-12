be40153e142b7271f5159a6a596e0f9c4f6a3131
Jen Poole <contact.vandepool@gmail.com>
2018-05-12 02:32:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,6 +13,22 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  # GET /goodbye/:name
+  # sends a 200 status code
+  # displays 'goodbye' and a name
+  # is not hard-coded
+  get '/goodbye/:name' do
+    @name = params[:name]
+    "Goodbye, #{@name}."
+  end
+
+  # GET /multiply/:num1/:num2
+  # sends a 200 status code
+  # displays the product of the two numbers in the route
+  # is not hard-coded
+  get "/multiply/:num1/:num2" do
+    @product = params[:num1].to_i * params[:num2].to_i
+    "#{@product}"
+  end
 
 end
