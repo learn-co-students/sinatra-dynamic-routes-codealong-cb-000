6c880e25a8e99a2bc48b1acb3870c1200f6134f6
Jason Viger <jason.viger@gmail.com>
2018-08-27 10:13:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,23 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
+    get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
 
   # Code your final two routes here:
+  get '/medicines/:id' do
+  @medicine = all_the_medicines.select do |medicine|
+    medicine.id == params[:id]
+  end.find
+  erb :'/medicines/show.html'
+  end
+  
+  get "/multiply/:num1/:num2" do
+    @product = params[:num1].to_i * params[:num2].to_i
+    "#{@product}"
+  end
+  
 
 end
